a2ba1423d2aa033027ab13cb44c5ef0f3964cda4
chid12345 <voskoboynikov.e.m@mail.ru>
2026-04-08 00:40:27 +0900

    v1.0.4: remove old tools and scripts

--- start_tma.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting Duel Arena TMA API server...
-echo Open in browser: http://localhost:8000
-echo.
-call .venv\Scripts\activate
-uvicorn api_server:app --host 0.0.0.0 --port 8000 --reload
-pause
